fb6c7bf2fdcc0afd24abe87da6942db973c6718c
fchabrun <46962341+fchabrun@users.noreply.github.com>
2025-05-31 19:08:02 +0200

    Create moist_dash.py

---
+++ moist_dash.py
@@ -0,0 +1,206 @@
+import os
+import argparse
+import pandas as pd
+from dash import Dash, html, dcc, Input, Output, callback, State
+import dash_bootstrap_components as dbc
+import plotly.graph_objs as go
+from plotly.subplots import make_subplots
+from datetime import datetime, timedelta
+import numpy as np
+import time
+import json
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--mode")
+parser.add_argument("--host")
+parser.add_argument("--port")
+parser.add_argument("--dash_ip", default="192.168.1.38")
+parser.add_argument("--rundir", default="/home/moist/moist_rundir")
+parser.add_argument("--auto_debug", default=True)
+parser.add_argument("--db_platform", default="mariadb")
+parser.add_argument("--db_host", default="localhost")
+parser.add_argument("--db_port", default=3306)
+parser.add_argument("--db_user", default="moist")
+parser.add_argument("--db_password", default="moisture")
+parser.add_argument("--db_database", default="moist")
+args = parser.parse_args()
+
+args.auto_debug = args.auto_debug is not None
+
+
+def now():
+    return datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+
+
+def log(s):
+    print(f"{now()}    {s}")
+
+
+if args.auto_debug and not os.path.exists(args.rundir):
+    args.dash_ip = "127.0.0.1"
+    # args.rundir = r"C:\Users\flori\OneDrive - univ-angers.fr\Documents\Home\Research\Common"
+    args.rundir = r"C:\Users\flori\OneDrive\Documents\moist_moist"
+
+if args.db_platform == "mariadb":
+    from sqlalchemy import create_engine
+
+
+def load_params_():
+    json_path = os.path.join(args.rundir, "settings.json")
+    with open(json_path, "r") as f:
+        params = json.load(f)
+    return params
+
+
+def save_params(params):
+    json_path = os.path.join(args.rundir, "settings.json")
+    with open(json_path, "w") as f:
+        json.dump(params, f, indent=4)
+
+
+def db_get_measurements_mariadb(minutes):
+    engine = create_engine(f"mariadb+mariadbconnector://{args.db_user}:{args.db_password}@{args.db_host}:{args.db_port}/{args.db_database}")
+    # engine = create_engine(f"mariadb:///?User={args.db_user}&;Password={args.db_password}&Database={args.db_database}&Server={args.db_host}&Port={args.db_port}")
+    dt_start = (datetime.now() - timedelta(minutes=minutes)).strftime('%Y-%m-%d %H:%M:%S')
+    dt_end = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+    output_data = pd.read_sql(f"SELECT * FROM moist_measurements WHERE time BETWEEN '{dt_start}' and '{dt_end}'", engine)
+    return output_data
+
+
+def fetch_db(minutes):
+    log("retrieving up-to-date db data")
+    # the real function
+    output_data, sensor_columns = None, []
+    if args.db_platform == "mariadb":
+        output_data = db_get_measurements_mariadb(minutes=minutes)
+    # format correctly
+    if output_data is not None:
+        output_data.time = pd.to_datetime(output_data.time)
+        sensor_columns = [col for col in output_data.columns.tolist() if col[:7] == "sensor_"]
+        output_data = output_data.astype({col: int for col in sensor_columns})
+    else:
+        print(f"unable to retrieve db data: unknown {args.db_platform}")
+    return output_data, sensor_columns
+
+
+def get_db_subset(db_extract: pd.DataFrame, events: list = ("entry", )):
+    return db_extract[db_extract.event.isin(events)]
+
+
+app = Dash(external_stylesheets=[dbc.themes.BOOTSTRAP])
+
+
+def draw_main_grap(time, sensor_values):
+    if len(time) == 0:
+        return None
+
+    fig = make_subplots()
+
+    limithi = 360
+    limitlo = 277
+
+    # Limits
+    fig.add_trace(
+        go.Scatter(x=time, y=limithi, name="Upper limit", line=dict(width=0.5, color='green'))
+    )
+    fig.add_trace(
+        go.Scatter(x=time, y=limitlo, name="Lower limit", line=dict(width=0.5, color='green'), fill='tonexty')
+    )
+
+    # Sensor measures
+    fig.add_trace(
+        go.Scatter(x=time, y=sensor_values, name="Humidity (raw)", line=dict(color='blue'))
+    )
+
+    # Set x-axis title
+    fig.update_xaxes(title_text="Time")
+
+    # get first y axis range
+    upper_x_limit = max(max(sensor_values), limithi) + 1
+    lower_x_limit = min(min(sensor_values), limitlo) - 1
+
+    # Set y-axes titles
+    fig.update_yaxes(title_text="Humidity (raw)", range=(lower_temp_limit, upper_temp_limit))
+
+    fig.update_layout(template="plotly_white", margin=dict(t=50, b=50))
+
+    return fig
+
+
+CONTENT_STYLE = {
+    "margin-left": "20rem",
+    "margin-right": "2rem",
+    "padding": "2rem 1rem",
+}
+
+content = html.Div(
+    [
+        html.Div([
+            html.H2(id="current-backend-status", className="lead"),
+            html.Hr(),
+            html.Div([
+                html.P("Display last (min)", style={"display": "inline-block", "width": "80%"}),
+                dcc.Input(min=1, max=1440, step=1, value=60, id='display-length-slider', type="number",
+                          style={"display": "inline-block", "width": "20%", "text-align": "right"}),
+                dbc.Switch(
+                    id="raw-switch",
+                    label="Display raw values",
+                    value=False,
+                ),
+                html.Button('Refresh', id='refresh-button', style={"width": "100%"}, n_clicks=0),
+            ]),
+            html.Hr(),
+            html.Div([dcc.Graph(id='sensor_0')]),
+            html.Div([dcc.Graph(id='sensor_1')]),
+            html.Div([dcc.Graph(id='sensor_2')]),
+            html.Div([dcc.Graph(id='sensor_3')]),
+            html.Div([dcc.Graph(id='sensor_4')]),
+            html.Div([dcc.Graph(id='sensor_5')]),
+        ], id='page-div', style={'width': '100%', 'display': 'block'}),
+    ], id="page-content", style=CONTENT_STYLE
+)
+
+app.layout = html.Div(
+    html.Div([
+        content,
+    ], id='main-div')
+)
+
+
+@callback(
+    Output('current-backend-status', 'children'),
+    Output('sensor_0', 'figure'),
+    Output('sensor_1', 'figure'),
+    Output('sensor_2', 'figure'),
+    Output('sensor_3', 'figure'),
+    Output('sensor_4', 'figure'),
+    Output('sensor_5', 'figure'),
+    Input('display-length-slider', 'value'),
+    Input('refresh-button', 'n_clicks'),
+    Input('raw-switch', 'value'),
+)
+def callback_update_from_db(param_minutes, n, raw_switch):
+    # extract db
+    db_extract = fetch_db(param_minutes)
+    db_extract_entries = get_db_subset(db_extract=db_extract, events=["entry",])
+
+    # figs
+    sensor_0_fig = draw_main_grap(time=db_extract_entries.time, sensor_values=db_extract_entries.sensor_0)
+    sensor_1_fig = draw_main_grap(time=db_extract_entries.time, sensor_values=db_extract_entries.sensor_1)
+    sensor_2_fig = draw_main_grap(time=db_extract_entries.time, sensor_values=db_extract_entries.sensor_2)
+    sensor_3_fig = draw_main_grap(time=db_extract_entries.time, sensor_values=db_extract_entries.sensor_3)
+    sensor_4_fig = draw_main_grap(time=db_extract_entries.time, sensor_values=db_extract_entries.sensor_4)
+    sensor_5_fig = draw_main_grap(time=db_extract_entries.time, sensor_values=db_extract_entries.sensor_5)
+
+    return (
+        sensor_0_fig,
+        sensor_1_fig,
+        sensor_2_fig,
+        sensor_3_fig,
+        sensor_4_fig,
+        sensor_5_fig,
+        )
+
+
+if __name__ == '__main__':
+    app.run(host=args.dash_ip)
