f710007fe2949d7c077b1aeb14307d2c76fe7512
fchabrun <46962341+fchabrun@users.noreply.github.com>
2025-05-31 17:10:51 +0200

    Create serial_test.py

---
+++ serial_test.py
@@ -0,0 +1,17 @@
+import serial
+import time
+
+serial = serial.Serial('/dev/tty0ACM0', 9600)
+
+last_send = time.time()
+
+while True:
+  serial.flushInput()
+  serial.flushOutput()
+  # output
+  if time.time() - last_send > 1:
+    last_send = time.time()
+    serial.print(b'1')
+  # input
+  rcom = serial.read()
+  print(f"Received: <{rcom}>")
