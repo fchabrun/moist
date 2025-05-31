9b765ebd972304267821b14895ad9c90b4f763a5
fchabrun <46962341+fchabrun@users.noreply.github.com>
2025-05-31 17:26:48 +0200

    Update serial_test.py

--- serial_test.py
+++ serial_test.py
@@ -1,17 +1,22 @@
 import serial
 import time
 
-serial_com = serial.Serial('/dev/ttyACM0', 9600)
+if __name__ == "__main__":
 
-last_send = -1
-
-while True:
-  serial_com.flushInput()
-  serial_com.flushOutput()
-  # output
-  if time.time() - last_send > 1:
-    last_send = time.time()
-    serial_com.print(b'1')
-  # input
-  rcom = serial_com.read()
-  print(f"Received: <{rcom}>")
+  serial_com = serial.Serial('/dev/ttyACM0', 9600)
+  serial_com.reset_input_buffer()
+  
+  last_send = -1
+  awaiting_reply = False
+  
+  while True:
+    # output
+    if (not awaiting_reply) and (time.time() - last_send > 1):
+      last_send = time.time()
+      serial_com.print(b'1')
+      awaiting_reply = True
+    # input
+    if serial_com.in_waiting > 0:
+        awaiting_reply = False
+        rcom = serial_com.readline().decode('utf-8').rstrip()
+        print(f"Received: <{rcom}>")
