524e2a08605ca33616613b8817b196f3fad07b95
fchabrun <46962341+fchabrun@users.noreply.github.com>
2025-05-31 21:14:58 +0200

    Update moist_dash.py

--- moist_dash.py
+++ moist_dash.py
@@ -142,7 +142,7 @@ content = html.Div(
             html.Hr(),
             html.Div([
                 html.P("Display last (min)", style={"display": "inline-block", "width": "80%"}),
-                dcc.Input(min=1, max=1080, step=1, value=1440, id='display-length-slider', type="number",
+                dcc.Input(min=1, max=10800, step=1, value=1440, id='display-length-slider', type="number",
                           style={"display": "inline-block", "width": "20%", "text-align": "right"}),
                 dbc.Switch(id="raw-switch", label="Display raw values", value=True),
                 html.Button('Refresh', id='refresh-button', style={"width": "100%"}, n_clicks=0),
