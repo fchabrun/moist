f46765d2b6ba9c163d0a9469ae604c1726478d43
fchabrun <46962341+fchabrun@users.noreply.github.com>
2025-06-01 13:29:09 +0200

    Create test_series.py

---
+++ test_series.py
@@ -0,0 +1,21 @@
+import pandas as pd
+
+def remap_value_by_state(val, state):
+    if state == ">3":
+        if val > 3:
+            return val
+        return np.nan
+    if state == "<=3":
+        if val <= 3:
+            return val
+        return np.nan
+
+test = pd.Series([1., 2, 3, 2, 1, 3, 6, 1, 2, 1, 3, 4, 5, 7, 9, 5, 11, 6, 2, 7, 4, 2, 1, 2, 3], name="sensor_0")
+
+print(f'{test=}')
+
+print(f"{test.map(lambda val: remap_value_by_state(val, state='>3'))=}")
+print(f"{test.map(lambda val: remap_value_by_state(val, state='<=3'))=}")
+
+print(f"{test.map(lambda val: remap_value_by_state(val, state='>3')).fillna(value=test, limit=1)=}")
+print(f"{test.map(lambda val: remap_value_by_state(val, state='<=3')).fillna(value=test, limit=1)=}")
